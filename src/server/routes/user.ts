7114f9c1cf614af4616ce25b5c198a3117650427
Joseph Kim <joseph.yp.kim@gmail.com>
2021-10-17 16:04:27 -0700

    Update getAll users route with service method call

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,21 +1,22 @@
-import * as express from "express";
-import userModel from "../database/models/user";
-import type { RequestHandler } from "express";
+import * as express from 'express';
+import userModel from '../database/models/user';
+import { userService } from '../services/';
+import type { RequestHandler } from 'express';
 
 export class UserController {
   private router: express.Router;
 
   constructor() {
     this.router = express.Router();
-    this.router.route("/").get(this.getAll);
+    this.router.route('/').get(this.getAll);
   }
 
   public initRoutes(apiRouter: express.Router) {
-    apiRouter.use("/user", this.router);
+    apiRouter.use('/users', this.router);
   }
 
   protected getAll: RequestHandler = async (req, res, next) => {
-    const users = await userModel.find({});
+    const users = await userService.getUsers();
     return res.json({ users });
   };
 }
