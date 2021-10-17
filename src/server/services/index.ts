cf397a27c3de801148dd1ba282aa5cf026f55d3a
Joseph Kim <joseph.yp.kim@gmail.com>
2021-10-17 16:02:58 -0700

    Initialize AlbumService

--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -1,10 +1,14 @@
 import { AlbumDao } from '../database/daos/AlbumDao';
 import { UserDao } from '../database/daos/UserDao';
+import { AlbumService } from './AlbumService';
 import { UserService } from './UserService';
 
+const albumDao = new AlbumDao();
 const userDao = new UserDao();
 
+const albumService = new AlbumService();
 const userService = new UserService();
+albumService.initialize({ albumDao });
 userService.initialize({ userDao });
 
-export { userService };
+export { albumService, userService };
