84685f7f697258beef5867c28de7e2e8247451a3
Joseph Kim <joseph.yp.kim@gmail.com>
2021-10-17 14:39:54 -0700

    Create UserDao Class

---
+++ src/server/database/daos/UserDao.ts
@@ -0,0 +1,15 @@
+import { Dao, DaoOptions, ID } from './dao';
+import UserModel from '../models/user';
+
+type User = {
+  id: ID;
+  email: string;
+  username: string;
+  password: string;
+};
+
+export class UserDao extends Dao<User> {
+  constructor(daoOptions?: DaoOptions<User>) {
+    super(UserModel, daoOptions);
+  }
+}
